f69c233f87ab8da29be864da57b63db010f60abf
MeanGold <jonathanmaenche@me.com>
2024-09-05 12:57:18 -0400

    Add input form to webpage

--- WebApp1/obj/Debug/net8.0/WebApp1.AssemblyInfo.cs
+++ WebApp1/obj/Debug/net8.0/WebApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f179db207e7db287bc95067e50a2d79c05c48845")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b95587962d2ef507861557b8ebf64716a6228d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
