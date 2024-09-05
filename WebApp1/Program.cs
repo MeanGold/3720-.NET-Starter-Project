b0b95587962d2ef507861557b8ebf64716a6228d
MeanGold <jonathanmaenche@me.com>
2024-09-05 12:28:55 -0400

    Adding MVC functionality

--- WebApp1/Program.cs
+++ WebApp1/Program.cs
@@ -1,7 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
 var app = builder.Build();
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
 app.UseDefaultFiles(); // Enables default file mapping, should be used before UseStaticFiles
+app.UseHttpsRedirection();
 app.UseStaticFiles(); // Enable serving static files
 
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=App}/{action=Index}/{id?}");
 app.Run();
