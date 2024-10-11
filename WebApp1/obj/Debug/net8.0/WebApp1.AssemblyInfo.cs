3c62936a6ded808f997f7f00a6c4fcd7d2b7c94e
Nicholas DiGennaro <113647565+ndigenn@users.noreply.github.com>
2024-10-11 10:59:56 -0400

    Create Account & Sign in page

--- WebApp1/obj/Debug/net8.0/WebApp1.AssemblyInfo.cs
+++ WebApp1/obj/Debug/net8.0/WebApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f69c233f87ab8da29be864da57b63db010f60abf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cde834cd25796f8a7c9b16548ec38a4f2c4c1d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
