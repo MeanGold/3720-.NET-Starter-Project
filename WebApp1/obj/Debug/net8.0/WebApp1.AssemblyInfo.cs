9bea5092ae8d46439657228a33d18a7d10cf3c90
MeanGold <jonathanmaenche@me.com>
2024-10-26 12:48:10 -0400

    test commit

--- WebApp1/obj/Debug/net8.0/WebApp1.AssemblyInfo.cs
+++ WebApp1/obj/Debug/net8.0/WebApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d7b4f36eb24c92a62a079ba5d5cd0d86f4d0dfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ec1e49a132deb48c015f1b1b31d4b433a8f3abf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
