dccd6c3224b4215ffb07b77b23099e8e4eac9892
MeanGold <jonathanmaenche@me.com>
2024-11-26 10:56:22 -0500

    Combining CSS for Events and details page into static CSS files in wwwroot

--- WebApp1/obj/Debug/net8.0/WebApp1.AssemblyInfo.cs
+++ WebApp1/obj/Debug/net8.0/WebApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4fffd4fbce713c53963b2ebf1bdbf3e159411e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f496218216d9b0d1d8ea31375d58e58949e5c4e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
